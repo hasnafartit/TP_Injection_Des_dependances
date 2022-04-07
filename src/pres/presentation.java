b53cf6166618a7b89b738155fcbd8caf89964109
hasnafartit <84719124+hasnafartit@users.noreply.github.com>
2022-04-07 23:57:39 +0000

    Add files via upload

---
+++ src/pres/presentation.java
@@ -0,0 +1,18 @@
+package pres;
+
+import dao.DaoImpl;
+import ext.DaoImpl2;
+import metier.MetierImpl;
+
+public class presentation {
+    public static void main(String[] args){
+        /*
+        Injection des dépendances par instanciation statique => new
+         */
+        //DaoImpl dao = new DaoImpl();
+        DaoImpl2 dao = new DaoImpl2();
+        MetierImpl metier = new MetierImpl();
+        metier.setDao(dao);
+        System.out.println("Résultat = "+metier.calcul());
+    }
+}
